820b5a9e802ccd0871e65d067d5dc69214e33ba5
EYAD2560 <aeyad2560@gmail.com>
2025-08-25 16:37:03 +0300

    m

--- lib/screens/anime/anime/display_popular/cubit/pagination/pagination_states.dart
+++ lib/screens/anime/anime/display_popular/cubit/pagination/pagination_states.dart
@@ -5,23 +5,30 @@ abstract class PopularAnimePaginationState<GlobalModel> {
   const PopularAnimePaginationState(this.items);
 }
 
-class PaginationInitial<GlobalModel> extends PopularAnimePaginationState<GlobalModel> {
+class PaginationInitial<GlobalModel>
+    extends PopularAnimePaginationState<GlobalModel> {
   const PaginationInitial() : super(const []);
 }
 
-class PopularAnimePaginationLoading<GlobalModel> extends PopularAnimePaginationState<GlobalModel> {
+class PopularAnimePaginationLoading<GlobalModel>
+    extends PopularAnimePaginationState<GlobalModel> {
   // ignore: use_super_parameters
   const PopularAnimePaginationLoading(List<GlobalModel> items) : super(items);
 }
 
-class PopularAnimePaginationLoaded<GlobalModel> extends PopularAnimePaginationState<GlobalModel> {
+class PopularAnimePaginationLoaded<GlobalModel>
+    extends PopularAnimePaginationState<GlobalModel> {
   final bool hasMore;
   // ignore: use_super_parameters
-  const PopularAnimePaginationLoaded(List<GlobalModel> items, {required this.hasMore}) : super(items);
+  const PopularAnimePaginationLoaded(List<GlobalModel> items,
+      {required this.hasMore})
+      : super(items);
 }
 
-class PopularAnimePaginationError<GlobalModel> extends PopularAnimePaginationState<GlobalModel> {
+class PopularAnimePaginationError<GlobalModel>
+    extends PopularAnimePaginationState<GlobalModel> {
   final String error;
   // ignore: use_super_parameters
-  const PopularAnimePaginationError(List<GlobalModel> items, this.error) : super(items);
+  const PopularAnimePaginationError(List<GlobalModel> items, this.error)
+      : super(items);
 }
